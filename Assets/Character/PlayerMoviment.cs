020ebdc76fb1f37b02f41c496ecdff54fad905b8
Trqz <joaomarcostroquez@gmail.com>
2023-04-12 12:13:47 +0100

    Adicao de movimentacao horizontal e gravidade

---
+++ Assets/Character/PlayerMoviment.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingpower = 16f;
+    private bool isfacingright = true;
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y >0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundcheck.position,0.2f ,groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isfacingright && horizontal <0f  || !isfacingright && horizontal > 0f)
+        {
+            isfacingright = !isfacingright;
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1f;
+            transform.localScale = localscale;
+        }
+    }
+}
