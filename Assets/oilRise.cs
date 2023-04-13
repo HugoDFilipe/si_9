59426fa8624ad6c1926f27a90590c302079fc89c
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2023-04-13 11:42:09 +0100

    Added Oil

---
+++ Assets/oilRise.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oilRise : MonoBehaviour
+{
+    [SerializeField]
+    private float oilSpeed = 1f;
+    public int keyNumber;
+
+    private bool rising = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyManager.KeyGet += checkKey;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rising)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + oilSpeed/1000 , 0);
+        }
+    }
+
+    void checkKey(int eventKeyNumber)
+    {
+        if (keyNumber == eventKeyNumber)
+        {
+            rising = true;
+        }
+    }
+}
