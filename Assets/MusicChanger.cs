d856bd7c9bcba98275a7821fc4410e5865e15749
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2023-04-13 20:57:20 +0100

    MusicAdd

---
+++ Assets/MusicChanger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    [SerializeField]
+    private int keyNumber;
+    [SerializeField]
+    private AudioClip actionSound;
+
+    private AudioSource audioSource;
+
+    private bool hasChanged = false;
+
+    void Start()
+    {
+        keyManager.KeyGet += checkKey;
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+
+
+    void checkKey(int eventKeyNumber)
+    {
+        if (keyNumber <= eventKeyNumber && !hasChanged)
+        {
+            audioSource.clip=actionSound;
+            audioSource.Play();
+            hasChanged = true;
+        }
+    }
+}
